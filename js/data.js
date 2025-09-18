5755a6e642ee82004b5cdf0680f430cc76959a44
Sergey Fortuna <serdiukgalina@mail.ru>
2025-09-19 01:59:15 +0700

    1updFix8

--- js/data.js
+++ js/data.js
@@ -8,112 +8,90 @@ export const data = [
         "imageURL": "./img/items/2.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Dragon",
         "imageURL": "./img/items/3.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Spirit",
         "imageURL": "./img/items/4.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Control",
         "imageURL": "./img/items/5.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Venom",
         "imageURL": "./img/items/6.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Shadow",
         "imageURL": "./img/items/7.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Dough",
         "imageURL": "./img/items/8.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "T-Rex",
         "imageURL": "./img/items/9.webp"
     },
     {
-        "bgColor": "#ea3131",
         "name": "Gravity",
         "imageURL": "./img/items/10.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Blizzard",
         "imageURL": "./img/items/11.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Pain",
         "imageURL": "./img/items/12.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Rumble",
         "imageURL": "./img/items/13.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Portal",
         "imageURL": "./img/items/14.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Phoenix",
         "imageURL": "./img/items/15.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Sound",
         "imageURL": "./img/items/16.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Spider",
         "imageURL": "./img/items/17.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Love",
         "imageURL": "./img/items/18.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Buddha",
         "imageURL": "./img/items/19.webp"
     },
     {
-        "bgColor": "#d12ddf",
         "name": "Quake",
         "imageURL": "./img/items/20.webp"
     },
     {
-        "bgColor": "#8b55fb",
         "name": "Magma",
         "imageURL": "./img/items/21.webp"
     },
     {
-        "bgColor": "#8b55fb",
         "name": "Barrier",
         "imageURL": "./img/items/22.webp"
     },
     {
-        "bgColor": "#8b55fb",
         "name": "Rubber",
         "imageURL": "./img/items/23.webp"
     },
     {
-        "bgColor": "#8b55fb",
         "name": "Light",
         "imageURL": "./img/items/24.webp"
     }
