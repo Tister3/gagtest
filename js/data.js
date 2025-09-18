1eb7008a95f11dd5cf1232dd6c676bf5a69a80c9
Sergey Fortuna <serdiukgalina@mail.ru>
2025-09-19 04:51:04 +0700

    2updFix1

--- js/data.js
+++ js/data.js
@@ -1,125 +1,189 @@
 export const data = [
     {
+        "name": "Golden Goose",
+        "imageURL": "./img/pets/Golden_Goose.webp"
+    },
+    {
+        "name": "Raccoon",
+        "imageURL": "./img/pets/Raccoon.webp"
+    },
+    {
+        "name": "Apple Gazelle",
+        "imageURL": "./img/pets/Apple_Gazelle.webp"
+    },
+    {
+        "name": "Blood Hedgehog",
+        "imageURL": "./img/pets/Blood_Hedgehog.webp"
+    },
+    {
+        "name": "Cockatrice",
+        "imageURL": "./img/pets/Cockatrice.webp"
+    },
+    {
+        "name": "Corrupted Kitsune",
+        "imageURL": "./img/pets/Corrupted_Kitsune.webp"
+    },
+    {
+        "name": "Disco Bee",
+        "imageURL": "./img/pets/Disco_Bee.webp"
+    },
+    {
+        "name": "Dragonfly",
+        "imageURL": "./img/pets/Dragonfly.webp"
+    },
+    {
+        "name": "French Fry Ferret",
+        "imageURL": "./img/pets/French_Fry_Ferret.webp"
+    },
+    {
+        "name": "Green Bean",
+        "imageURL": "./img/pets/Green_Bean.webp"
+    },
+    {
+        "name": "Kappa",
+        "imageURL": "./img/pets/Kappa.webp"
+    },
+    {
         "name": "Kitsune",
-        "imageURL": "./img/items/1.webp"
+        "imageURL": "./img/pets/Kitsune.webp"
     },
     {
-        "name": "Leopard",
-        "imageURL": "./img/items/2.webp"
+        "name": "Lemon Lion",
+        "imageURL": "./img/pets/Lemon_Lion.webp"
     },
     {
-        "name": "Dragon",
-        "imageURL": "./img/items/3.webp"
+        "name": "Lobster Thermidor",
+        "imageURL": "./img/pets/Lobster_Thermidor.webp"
     },
     {
-        "name": "Spirit",
-        "imageURL": "./img/items/4.webp"
+        "name": "Moon Cat",
+        "imageURL": "./img/pets/Moon_Cat.webp"
     },
     {
-        "name": "Control",
-        "imageURL": "./img/items/5.webp"
+        "name": "Peach Wasp",
+        "imageURL": "./img/pets/Peach_Wasp.webp"
     },
     {
-        "name": "Venom",
-        "imageURL": "./img/items/6.webp"
+        "name": "Raiju",
+        "imageURL": "./img/pets/Raiju.webp"
     },
     {
-        "name": "Shadow",
-        "imageURL": "./img/items/7.webp"
+        "name": "Spaghetti Sloth",
+        "imageURL": "./img/pets/Spaghetti_Sloth.webp"
     },
     {
-        "name": "Dough",
-        "imageURL": "./img/items/8.webp"
+        "name": "Spinosaurus",
+        "imageURL": "./img/pets/Spinosaurus.webp"
+    },
+    {
+        "name": "Butterfly",
+        "imageURL": "./img/pets/Butterfly.webp"
     },
     {
         "name": "T-Rex",
-        "imageURL": "./img/items/9.webp"
-    },
-    {
-        "name": "Gravity",
-        "imageURL": "./img/items/10.webp"
-    },
-    {
-        "name": "Blizzard",
-        "imageURL": "./img/items/11.webp"
-    },
-    {
-        "name": "Pain",
-        "imageURL": "./img/items/12.webp"
-    },
-    {
-        "name": "Rumble",
-        "imageURL": "./img/items/13.webp"
-    },
-    {
-        "name": "Portal",
-        "imageURL": "./img/items/14.webp"
-    },
-    {
-        "name": "Phoenix",
-        "imageURL": "./img/items/15.webp"
-    },
-    {
-        "name": "Sound",
-        "imageURL": "./img/items/16.webp"
-    },
-    {
-        "name": "Spider",
-        "imageURL": "./img/items/17.webp"
-    },
-    {
-        "name": "Love",
-        "imageURL": "./img/items/18.webp"
-    },
-    {
-        "name": "Buddha",
-        "imageURL": "./img/items/19.webp"
-    },
-    {
-        "name": "Quake",
-        "imageURL": "./img/items/20.webp"
-    },
-    {
-        "name": "Magma",
-        "imageURL": "./img/items/21.webp"
-    },
-    {
-        "name": "Barrier",
-        "imageURL": "./img/items/22.webp"
-    },
-    {
-        "name": "Rubber",
-        "imageURL": "./img/items/23.webp"
-    },
-    {
-        "name": "Light",
-        "imageURL": "./img/items/24.webp"
+        "imageURL": "./img/pets/T-Rex.webp"
     }
 ]
 
-export const petsFiles = [
-"T-Rex.webp",
-"Apple_Gazelle.webp",
-"Blood_Hedgehog.webp",
-"Cockatrice.webp",
-"Corrupted_Kitsune.webp",
-"Disco_Bee.webp",
-"Dragonfly.webp",
-"French_Fry_Ferret.webp",
-"Golden_Goose.webp",
-"Green_Bean.webp",
-"Kappa.webp",
-"Kitsune.webp",
-"Lemon_Lion.webp",
-"Lobster_Thermidor.webp",
-"Moon_Cat.webp",
-"Peach_Wasp.webp",
-"Raccon.webp",
-"Raiju.webp",
-"Spaghetti_Sloth.webp",
-"Spinosaurus.webp",
-"Thy_Butterfly.webp",
-];
+export const fruits = [
+    {
+        "name": "Kitsune",
+        "imageURL": "./img/fruits/1.webp"
+    },
+    {
+        "name": "Leopard",
+        "imageURL": "./img/fruits/2.webp"
+    },
+    {
+        "name": "Dragon",
+        "imageURL": "./img/fruits/3.webp"
+    },
+    {
+        "name": "Spirit",
+        "imageURL": "./img/fruits/4.webp"
+    },
+    {
+        "name": "Control",
+        "imageURL": "./img/fruits/5.webp"
+    },
+    {
+        "name": "Venom",
+        "imageURL": "./img/fruits/6.webp"
+    },
+    {
+        "name": "Shadow",
+        "imageURL": "./img/fruits/7.webp"
+    },
+    {
+        "name": "Dough",
+        "imageURL": "./img/fruits/8.webp"
+    },
+    {
+        "name": "T-Rex",
+        "imageURL": "./img/fruits/9.webp"
+    },
+    {
+        "name": "Gravity",
+        "imageURL": "./img/fruits/10.webp"
+    },
+    {
+        "name": "Blizzard",
+        "imageURL": "./img/fruits/11.webp"
+    },
+    {
+        "name": "Pain",
+        "imageURL": "./img/fruits/12.webp"
+    },
+    {
+        "name": "Rumble",
+        "imageURL": "./img/fruits/13.webp"
+    },
+    {
+        "name": "Portal",
+        "imageURL": "./img/fruits/14.webp"
+    },
+    {
+        "name": "Phoenix",
+        "imageURL": "./img/fruits/15.webp"
+    },
+    {
+        "name": "Sound",
+        "imageURL": "./img/fruits/16.webp"
+    },
+    {
+        "name": "Spider",
+        "imageURL": "./img/fruits/17.webp"
+    },
+    {
+        "name": "Love",
+        "imageURL": "./img/fruits/18.webp"
+    },
+    {
+        "name": "Buddha",
+        "imageURL": "./img/fruits/19.webp"
+    },
+    {
+        "name": "Quake",
+        "imageURL": "./img/fruits/20.webp"
+    },
+    {
+        "name": "Magma",
+        "imageURL": "./img/fruits/21.webp"
+    },
+    {
+        "name": "Barrier",
+        "imageURL": "./img/fruits/22.webp"
+    },
+    {
+        "name": "Rubber",
+        "imageURL": "./img/fruits/23.webp"
+    },
+    {
+        "name": "Light",
+        "imageURL": "./img/fruits/24.webp"
+    }
+]
+
 
 export const fruitsFiles = [
 "Wispwing.webp",
